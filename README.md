26093f8aec6fbde4cb6cfc692c383313bf3edf41
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-04-27 16:57:13 +0200

    added files

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # css-transform-string
-Convert js object to css transform string
+
+[![npm version](https://img.shields.io/npm/v/css-transform-string.svg?style=flat)](https://www.npmjs.com/package/css-transform-string)
+
+### transform(_transformObject_)
+
+* `transformObject`: object
+  * `x`: number | string,
+  * `y`: number | string,
+  * `z`: number | string
+  * `translate`: number | string | array (number | string)
+  * `translate3d`: number | string | array (number | string)
+  * `scale`: number | array (number)
+  * `scaleX`: number
+  * `scaleY`: number
+  * `scaleZ`: number
+  * `scale3d`: number | array (number)
+  * `rotate`: number | string
+  * `rotateX`: number | string
+  * `rotateY`: number | string
+  * `rotateZ`: number | string
+  * `skew`: number | string | array (number | string)
+  * `perspective`: number | string
+* returns: string
+
+Returns css string of the input object (all properties optional).
+
+```js
+import transform from "css-transform-string";
+
+transform({ x: 50, y: "50%", rotateX: 10, rotateY: "1rad" });
+// "translateX(50px) translateY(50%) rotateX(10deg) rotateY(1rad)"
+```
---
+++ index.js
@@ -0,0 +1,87 @@
+function parseCoord(translate) {
+  return typeof translate === 'string'
+    ? translate
+    : translate + (translate > 0 ? 'px' : '');
+}
+
+function parseDegree(degree) {
+  return typeof degree === 'string'
+    ? degree
+    : degree + (degree > 0 ? 'deg' : '');
+}
+
+function getMultiDimensionalString(coords, parser) {
+  return coords.reduce((accum, coord, i) => {
+    accum += parser(coord);
+    accum += i < coords.length - 1 ? ', ' : '';
+    return accum;
+  }, '');
+}
+
+function getMultiTranslate(coords) {
+  return coords.length > 1
+    ? getMultiDimensionalString(coords, parseCoord)
+    : parseCoord(coords);
+}
+
+function getMultiDegrees(degrees) {
+  return degrees.length > 1
+    ? getMultiDimensionalString(degrees, parseDegree)
+    : parseDegree(degrees);
+}
+
+function getMultiValues(values) {
+  return values.length > 1 ? getMultiDimensionalString(values, v => v) : values;
+}
+
+function exists(val) {
+  return typeof val !== 'undefined';
+}
+
+function transform({
+  x,
+  y,
+  z,
+  translate,
+  translate3d,
+  scale,
+  scaleX,
+  scaleY,
+  scaleZ,
+  scale3d,
+  rotate,
+  rotateX,
+  rotateY,
+  rotateZ,
+  skew,
+  perspective
+}) {
+  let string = '';
+
+  string += exists(x) ? `translateX(${parseCoord(x)}) ` : '';
+  string += exists(y) ? `translateY(${parseCoord(y)}) ` : '';
+  string += exists(z) ? `translateZ(${parseCoord(z)}) ` : '';
+  string += exists(translate)
+    ? `translate(${getMultiTranslate(translate)}) `
+    : '';
+  string += exists(translate3d)
+    ? `translate3d(${getMultiTranslate(translate3d)}) `
+    : '';
+  string += exists(scale) ? `scale(${getMultiValues(scale)}) ` : '';
+  string += exists(scaleX) ? `scaleX(${scaleX}) ` : '';
+  string += exists(scaleY) ? `scaleY(${scaleY}) ` : '';
+  string += exists(scaleZ) ? `scaleZ(${scaleZ}) ` : '';
+  string += exists(scale3d) ? `scale3d(${getMultiValues(scale3d)}) ` : '';
+  string += exists(rotate) ? `rotate(${parseDegree(rotate)}) ` : '';
+  string += exists(rotateX) ? `rotateX(${parseDegree(rotateX)}) ` : '';
+  string += exists(rotateY) ? `rotateY(${parseDegree(rotateY)}) ` : '';
+  string += exists(rotateZ) ? `rotateZ(${parseDegree(rotateZ)}) ` : '';
+  string += exists(skew) ? `skew(${getMultiDegrees(skew)}) ` : '';
+  string += exists(perspective)
+    ? `perspective(${parseCoord(perspective)}) `
+    : '';
+
+  return string;
+}
+
+export default transform;
