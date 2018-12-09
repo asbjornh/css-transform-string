97a61f6d14955e771ac0e92136b1bc489433859f
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2018-12-09 13:57:21 +0100

    Renames functions

--- index.js
+++ index.js
@@ -4,35 +4,39 @@ const maybeAddUnit = (value, unit) =>
 const maybePixels = value => maybeAddUnit(value, 'px');
 const maybeDegrees = value => maybeAddUnit(value, 'deg');
 
+// If passed an array, returns that array.
+// If passed a non-array, returns an array containing that value.
 const ensureArray = value => (value.length ? value : [value]);
 
 const stringifyList = (list, unit) =>
   list.map(value => maybeAddUnit(value, unit)).join(', ');
 
-const getMultiTranslate = coords => stringifyList(ensureArray(coords), 'px');
-const getMultiDegrees = coords => stringifyList(ensureArray(coords), 'deg');
-const getMultiUnitless = coords => stringifyList(ensureArray(coords), '');
+const maybePixelsList = coords => stringifyList(ensureArray(coords), 'px');
+const maybeDegreesList = coords => stringifyList(ensureArray(coords), 'deg');
+const unitlessList = coords => stringifyList(ensureArray(coords), '');
 
 const stringifiers = {
   x: v => `translateX(${maybePixels(v)})`,
   y: v => `translateY(${maybePixels(v)})`,
   z: v => `translateZ(${maybePixels(v)})`,
-  translate: v => `translate(${getMultiTranslate(v)})`,
-  translate3d: v => `translate3d(${getMultiTranslate(v)})`,
-  scale: v => `scale(${getMultiUnitless(v)})`,
+  translate: v => `translate(${maybePixelsList(v)})`,
+  translate3d: v => `translate3d(${maybePixelsList(v)})`,
+  scale: v => `scale(${unitlessList(v)})`,
   scaleX: v => `scaleX(${v})`,
   scaleY: v => `scaleY(${v})`,
   scaleZ: v => `scaleZ(${v})`,
-  scale3d: v => `scale3d(${getMultiUnitless(v)})`,
+  scale3d: v => `scale3d(${unitlessList(v)})`,
   rotate: v => `rotate(${maybeDegrees(v)})`,
   rotateX: v => `rotateX(${maybeDegrees(v)})`,
   rotateY: v => `rotateY(${maybeDegrees(v)})`,
   rotateZ: v => `rotateZ(${maybeDegrees(v)})`,
-  skew: v => `skew(${getMultiDegrees(v)})`,
+  skew: v => `skew(${maybeDegreesList(v)})`,
+  skewX: v => `skewX(${maybeDegrees(v)})`,
+  skewY: v => `skewY(${maybeDegrees(v)})`,
   perspective: v => `perspective(${maybePixels(v)})`
 };
 
-function transform(transformProperties) {
+function transform(transformProperties = {}) {
   return Object.entries(transformProperties)
     .map(([name, value]) => {
       const stringifier = stringifiers[name];
